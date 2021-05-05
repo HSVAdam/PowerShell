5c6bc17c02ee76646da0dc77723d720be5111e22
Adam Branham <adam.j.branham@gmail.com>
2021-05-05 11:44:43 -0500

    Adding Get-ScriptDirectory.ps1

---
+++ Functions/Get-ScriptDirectory.ps1
@@ -0,0 +1,6 @@
+﻿FUNCTION Get-ScriptDirectory
+{
+	$Invocation = (Get-Variable MyInvocation -Scope 1).Value
+	$Global:ScriptName = $Invocation.MyCommand.Path
+	RETURN Split-Path $Invocation.MyCommand.Path -Parent
+}
