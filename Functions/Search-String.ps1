a68410efad50e5f7ab6b7e857b0ff5e9dfb2c604
Adam Branham <adam.j.branham@gmail.com>
2021-05-07 12:15:00 -0500

    Adding Search-String.ps1

---
+++ Functions/Search-String.ps1
@@ -0,0 +1,22 @@
+﻿FUNCTION Search-String
+{
+	[CmdletBinding()]
+	PARAM (
+		[Parameter(Mandatory = $true, Position = 1)]
+		[string]$Folder,
+		[Parameter(Mandatory = $true, Position = 2)]
+		[string]$Search,
+		[Parameter(Mandatory = $false, Position = 2)]
+		[bool]$Recurse = $false
+	)
+	
+	IF ($Recurse -eq $true)
+	{
+		Get-ChildItem -Path $Folder -Recurse | Select-String $Search | Select-Object LineNumber, Path, @{ Name = "LineText"; Expression = { $_.Line.Trim() } } | Sort-Object Path
+		
+	}
+	ELSE
+	{
+		Get-ChildItem -Path $Folder | Select-String $Search | Select-Object LineNumber, Path, @{ Name = "LineText"; Expression = { $_.Line.Trim() } } | Sort-Object Path
+	}
+}
